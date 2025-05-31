e56c4bbacd91289abb865be74153610d415d0d68
taekkong <taekyung630@naver.com>
2025-05-31 15:02:35 +0900

    ~t20 Pass

--- src/main/java/me/kimtaekyung/Calc.java
+++ src/main/java/me/kimtaekyung/Calc.java
@@ -1,9 +1,12 @@
 package me.kimtaekyung;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Calc {
 
-    static int run(String str){
-
+    /*static int run(String str){
         String[] strSplit = str.trim().split(" ");
 
         int result=getInt(strSplit[0]);
@@ -24,8 +27,49 @@ public class Calc {
         }
 
         return result;
+    }*/
+
+    static int run(String str){
+        str=str.replaceAll("[()]","");
+        String[] strSplit=str.trim().split(" ");
+        List<String> strList = new ArrayList<>(Arrays.asList(strSplit));
+
+        int result = calc(strList);
+
+        return result;
     }
 
+    static int calc(List<String> list) {
+
+        for (int i = 1; i < list.size(); i += 2) {
+            if(list.get(i).equals("*")){
+                list.set(i - 1, String.valueOf(getInt(list.get(i - 1)) * getInt(list.get(i + 1))));
+                list.remove(i+1);
+                list.remove(i);
+                 return calc(list);
+            }else if(list.get(i).equals("/")){
+                list.set(i - 1, String.valueOf(getInt(list.get(i - 1)) / getInt(list.get(i + 1))));
+                list.remove(i+1);
+                list.remove(i);
+                return calc(list);
+            }
+        }
+        int result=getInt(list.get(0));
+
+        for(int i=1;i<list.size();i+=2){
+            int o = getInt(list.get(i + 1));
+            switch (list.get(i)){
+                case "+":
+                    result+=o;
+                    break;
+                case "-":
+                    result-=o;
+                    break;
+            }
+        }
+        return result;
+
+    }
     static int getInt(String str){
         return Integer.parseInt(str.trim());
     }
--- src/test/java/me/kimtaekyung/CalcTest.java
+++ src/test/java/me/kimtaekyung/CalcTest.java
@@ -91,4 +91,39 @@ public class CalcTest {
         assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
     }
 
+    @Test
+    @DisplayName("10 + 5 * 2 == 20")
+    void t15() {
+        assertThat(Calc.run("10 + 5 * 2")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("20 + 10 + 5 * 2 == 40")
+    void t16() {
+        assertThat(Calc.run("20 + 10 + 5 * 2")).isEqualTo(40);
+    }
+
+    @Test
+    @DisplayName("10 * 20 + 10 + 5 * 2 == 220")
+    void t17() {
+        assertThat(Calc.run("10 * 20 + 10 + 5 * 2")).isEqualTo(220);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) == 30")
+    void t18() {
+        assertThat(Calc.run("(10 + 20)")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("((10 + 20)) == 30")
+    void t19() {
+        assertThat(Calc.run("((10 + 20))")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("(((10 + 20))) == 30")
+    void t20() {
+        assertThat(Calc.run("(((10 + 20)))")).isEqualTo(30);
+    }
 }
